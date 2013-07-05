c92fb9ff8b2bd9a1a31b897b7bd71c50473b479d
David Coss <David.Coss@stjude.org>
2013-07-05 09:47:24 -0500

    Updated test to include running gsub on a submission script.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-from setuptools import setup,Command
+from setuptools import setup, Command
+
 
 class Tester(Command):
     user_options = []
@@ -15,22 +16,31 @@ class Tester(Command):
     def run(self):
         import test
         print("Testing dag")
-        if test.run():
+        if test.test_dag_objects():
             print("Success")
         else:
             print("Failure")
+            exit(1)
+
+        print("Testing gsub")
+        if test.test_gsub():
+            print("Success")
+        else:
+            print("Failure")
+            exit(1)
 
         print("Testing progress bar")
-        test.test_progress_bar()
+        #test.test_progress_bar()
         print("Did you see a progress bar?")
- 
-setup (name ='dag',
-       version = '1.4.0',
-       description = 'DAG Batch Job Creator for BOINC',
-       author = 'David Coss, PhD',
-       author_email = 'David.Coss@stjude.org',
-       license = 'GPL v3',
-       packages = ['dag'],
-       scripts = ["scripts/gsub", "scripts/update_dag"],
-       cmdclass ={'test':Tester}
-       )
+
+
+setup(name='dag',
+      version='1.4.0',
+      description='DAG Batch Job Creator for BOINC',
+      author='David Coss, PhD',
+      author_email='David.Coss@stjude.org',
+      license='GPL v3',
+      packages=['dag'],
+      scripts=["scripts/gsub", "scripts/update_dag"],
+      cmdclass={'test': Tester}
+      )
--- test/__init__.py
+++ test/__init__.py
@@ -1,5 +1,5 @@
+def test_dag_objects():
 
-def run():
     def end(retval):
         os.chdir(orig_dir)
         return retval
@@ -9,14 +9,18 @@ def run():
     import os
     orig_dir = os.getcwd()
     os.chdir("test")
-    
+
     # Create DAG
     d = dag.DAG()
-    p = dag.Process("test",[],[],"-arg1 -arg2 FILE")
+    p = dag.Process()
+    p.cmd = "test"
+    p.input_files = []
+    p.output_files = []
+    p.args = "-arg1 -arg2 FILE"
     p.workunit_name = "test1"
     p2 = p
     p2.workunit_name = "test2"
-    for i in [p,p2]:
+    for i in [p, p2]:
         d.add_process(i)
 
     # Save DAG
@@ -24,7 +28,7 @@ def run():
 
     # Load DAG
     d2 = dag.load(dag_filename)
-    
+
     # Do processes in DAGs match?
     procs = d2.processes
     if len(procs) != 2:
@@ -33,10 +37,10 @@ def run():
         return end(False)
 
     for proc in procs:
-        for attr in ["workunit_name","cmd","args"]:
+        for attr in ["workunit_name", "cmd", "args"]:
             matches = False
-            for orig_proc in [p,p2]:
-                if getattr(proc,attr) == getattr(orig_proc,attr):
+            for orig_proc in [p, p2]:
+                if getattr(proc, attr) == getattr(orig_proc, attr):
                     matches = True
                     break
             if not matches:
@@ -47,14 +51,44 @@ def run():
                 return end(False)
 
     os.unlink(dag_filename)
-    
+
     return end(True)
+
+
+def test_gsub():
+    from dag import gsub, DEFAULT_DAGFILE_NAME
+    from os.path import isfile
+
+    expected_command = "print(\"Not a comment\")"
+
+    if isfile(DEFAULT_DAGFILE_NAME):
+        from os import unlink
+        unlink(DEFAULT_DAGFILE_NAME)
+    test_dag = gsub.gsub("test/internal.sub", init_filename="test/dagrc")
+    processes = test_dag.processes
+    if len(processes) != 1:
+        print("Invalid number of processes produced by submission file")
+        print("Expected 1 but have {0}".format(len(processes)))
+        return False
+
+    process = processes[0]
+    if process.cmd != expected_command:
+        print("Command parsed from file is invalid.")
+        print("Expected: {0}".format(expected_command))
+        print("Have: {0}".format(process.cmd))
+        return False
+
+    return True
+
 
 def test_progress_bar():
     from progressbar import ProgressBar, Percentage, Bar
     from time import sleep
-    progress_bar = ProgressBar(widgets = [Percentage(), Bar()], maxval=10).start()
-    for i in range(1,10):
+    import os
+
+    progress_bar = ProgressBar(widgets=[Percentage(), Bar()],
+                               maxval=10).start()
+    for i in range(1, 5):
         progress_bar.update(i)
         sleep(1)
     print("")
