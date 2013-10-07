925d607b35661b342c3c8bb3499ed1b717466523
David Coss <David.Coss@stjude.org>
2013-10-07 13:11:47 -0500

    Added check for progress bar initialization.

--- dag/update_dag.py
+++ dag/update_dag.py
@@ -231,7 +231,9 @@ def update_dag(cmd, cmd_args, dagfile = dag.DEFAULT_DAGFILE_NAME, debug = False)
                     progress_bar = None
                     if not debug:
                         from progressbar import ProgressBar, Percentage, Bar
-                        progress_bar = ProgressBar(widgets = [Percentage(), Bar()], maxval=len(root_dag.processes)).start()
+                        num_processes = len(root_dag.processes)
+                        if num_processes:
+                            progress_bar = ProgressBar(widgets = [Percentage(), Bar()], maxval=num_processes).start()
                     for proc in root_dag.processes:
                         if debug:
                             print("Removing %s" % proc.workunit_name)
