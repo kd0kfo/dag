b1e93ef21bfb8719b49f91b641889af4b5d09d4e
David Coss <David.Coss@stjude.org>
2014-03-24 10:46:36 -0500

    Prototyped manual use of threads in dag.shell.
    multiprocessing in python is terrible.

--- dag/shell.py
+++ dag/shell.py
@@ -13,12 +13,20 @@ Interface module to POSIX shells. This allows processes in a DAG to interpret
 """
 
 from dag import Process, States, strstate
+import logging
+
+L = logging.getLogger("dag.shell")
+
 DEFAULT_NUMBER_OF_CORES = 1
 
 # Messages Queue Fields
 QUEUE_NAME = "dag"
 CLI_SENDER_PREFIX = "cli"
 MASTER_SENDER_NAME = "master"
+KILL_SIGNAL = "kill"
+
+kill_switch = False
+running_children = []
 
 
 class ShellProcess(Process):
@@ -41,18 +49,36 @@ class ShellProcess(Process):
 
     def start(self):
         import subprocess
-        
+        import time
+        import smq
+
+        message_queue = self.message_queue
+
         def set_niceness():
             if self.nice:
                 from os import nice
-                print("Changing niceness by %d" % self.nice)
+                L.info("Changing niceness by %d" % self.nice)
                 nice(self.nice)
 
-        print("Starting {0}".format(self.cmd))
+        L.info("Starting {0}".format(self.cmd))
         stdout_file = open("%s.stdout" % self.workunit_name, "w")
         stderr_file = open("%s.stderr" % self.workunit_name, "w")
-        retval = subprocess.call([self.cmd] + self.args, preexec_fn=set_niceness, stdout=stdout_file, stderr=stderr_file)
-        print("{0} Finished".format(self.cmd))
+        # retval = subprocess.call([self.cmd] + self.args, preexec_fn=set_niceness, stdout=stdout_file, stderr=stderr_file)
+        shell_process = subprocess.Popen([self.cmd] + self.args, preexec_fn=set_niceness, stdout=stdout_file, stderr=stderr_file)
+        while shell_process.poll() is None:
+            if message_queue.has_message(self.workunit_name):
+                message = message_queue.next(self.workunit_name)
+                if message.content == KILL_SIGNAL:
+                    L.debug("%s got kill signal" % self.workunit_name)
+                    shell_process.terminate()
+                    shell_process.wait()
+                    for F in (stdout_file, stderr_file):
+                        F.close()
+                    break
+            time.sleep(5)
+        L.info("{0} Finished".format(self.cmd))
+        message_queue.send(smq.Message("state:%d" % self.state, "str",
+                               self.workunit_name, MASTER_SENDER_NAME))
 
 
 class Waiter(ShellProcess):
@@ -74,10 +100,10 @@ class Waiter(ShellProcess):
             return True
 
         pid = int(self.args[0])
-        print("Waiting on pid %d" % pid)
+        L.debug("Waiting on pid %d" % pid)
         while check_pid(pid):
             time.sleep(self.POLL_PERIOD)
-        print("No longer waiting on pid %d" % pid)
+        L.debug("No longer waiting on pid %d" % pid)
 
 
 def parse_shell(cmd, args, header_map, parsers, init_code=None):
@@ -94,11 +120,15 @@ def parse_shell(cmd, args, header_map, parsers, init_code=None):
     return proc_list
 
 
-def runprocess(proc, queue):
-    print("Starting %s" % proc.workunit_name)
+def runprocess(proc, message_queue):
+    """
+    from os import getpid
+    pid = getpid()
+    print("Starting %s with pid %d" % (proc.workunit_name, pid))
     try:
         proc.state = States.RUNNING
-        queue.put((proc.workunit_name, proc.state))
+        proc.queue_filename = message_queue
+        queue.put((proc.workunit_name, proc.state, pid))
         proc.start()
         proc.state = States.SUCCESS
     except Exception as e:
@@ -106,12 +136,20 @@ def runprocess(proc, queue):
         print("TYPE: %s" % type(proc))
         print(e)
         proc.state = States.FAIL
-    queue.put((proc.workunit_name, proc.state))
+    queue.put((proc.workunit_name, proc.state, pid))
     return proc
+    """
+    import os
+    pid = os.fork()
+    if pid:  # Master
+        return pid
 
-
-def callback(proc):
-    print("Finished:%s State: %s" % (proc.workunit_name, strstate(proc.state)))
+    # Child
+    L.debug("Forked %s as %d" % (proc.workunit_name, os.getpid()))
+    proc.message_queue = message_queue
+    proc.start()
+    L.debug("%d finished start()" % os.getpid())
+    exit(0)
 
 
 def perform_operation(root_dag, message):
@@ -126,7 +164,7 @@ def perform_operation(root_dag, message):
     if cmd in ["start"]:
         return "Shell command line client cannot run %s" % cmd
 
-    print("Shell Monitor is running %s" % cmd)
+    L.debug("Shell Monitor is running %s" % cmd)
     try:
         retval = modify_dag(root_dag, cmd, cmd_args, True)
         root_dag.save()
@@ -137,67 +175,83 @@ def perform_operation(root_dag, message):
 
 
 def process_messages(root_dag, message_queue):
+    global kill_switch
     from smq import Message
+
+    def send(text, recipient):
+        message_queue.send(Message(text, "str", MASTER_SENDER_NAME, recipient))
+
     while message_queue.has_message(MASTER_SENDER_NAME):
         message = message_queue.next(MASTER_SENDER_NAME)
-        print("Processing Message from %s" % message.sender)
-        retval = perform_operation(root_dag, message)
-        message_queue.send(Message(retval, "str", MASTER_SENDER_NAME, message.sender))
+        L.debug("Processing Message from %s" % message.sender)
+        if message.content == "shutdown":
+            kill_switch = True
+            retval = "Shutting down shell processes"
+        elif message.content.startswith("state:"):
+            proc = root_dag.get_process(message.sender)
+            if not proc:
+                retval = ("Cannot change state. Unknown process %s"
+                          % message.sender)
+                break
+            newstate = message.content.replace("state:", "")
+            proc.state = int(newstate)
+            retval = ("Changed state of %s to %s"
+                      % (proc.workunit_name, strstate(proc.state)))
+            root_dag.save()
+        else:
+            retval = perform_operation(root_dag, message)
+        send(retval, message.sender)
+
 
 def create_work(root_dag, dag_path):
-    from multiprocessing import Manager, Pool
     import smq
+    from os import getpid
 
+    global kill_switch
+    global running_children
+    
     if root_dag.num_cores:
         num_cores = root_dag.num_cores
     else:
         num_cores = DEFAULT_NUMBER_OF_CORES
-    pool = Pool(num_cores)
-    manager = Manager()
-    # this queue is used by this python process and the worker threads
-    thread_queue = manager.Queue()
+    master_pid = getpid()  # called before fork. All are therefore aware who master is.
 
     # Setup Message queue used for update_dag to communicate with this
     # python thread, rather than directly modify root_dag
-    
     message_queue = smq.Queue(QUEUE_NAME, root_dag.queue_filename, timeout=7)
     process_messages(root_dag, message_queue)
 
-    torun = root_dag.generate_runnable_list()    
+    torun = root_dag.generate_runnable_list()
     if not torun:
-        pool.close()
         return
 
     # doing loop so that in the future finished processes
     # may start other processes
     import time
-    from os import nice
     waiting_states = (States.CREATED, States.STAGED)
     num_processes_left = len(root_dag.get_processes_by_state(waiting_states))
-    print("Doing work locally with %d cores" % num_cores)
+    L.info("Doing work locally with %d cores. Master PID %d" % (num_cores, getpid()))
     try:
         while torun or num_processes_left:
-            should_save_dag = not thread_queue.empty()
             for process in torun:
-                should_save_dag = True
-                pool.apply_async(runprocess, (process, thread_queue,), callback=callback)
-            time.sleep(5)
-            while not thread_queue.empty():
-                (procname, state) = thread_queue.get()
-                finished_process = root_dag.get_process(procname)
-                if not finished_process:
-                    continue
-                finished_process.state = state
-                should_save_dag = True
-            torun = root_dag.generate_runnable_list()
+                if len(running_children) >= num_cores:
+                    break
+                pid = runprocess(process, message_queue)
+                running_children.append((process.workunit_name, pid))
             num_processes_left = len(root_dag.get_processes_by_state(waiting_states))
-            if should_save_dag:
-                root_dag.save()
             process_messages(root_dag, message_queue)
-    except KeyboardInterrupt as ki:
-        pool.terminate()
-    pool.close()
-    pool.join()
+            if kill_switch:
+                break
+            torun = root_dag.generate_runnable_list()
+            time.sleep(5)
+    finally:
+        mypid = getpid()
+        if mypid == master_pid and running_children:
+            from os import kill, getpid
+            L.debug("%d is killing threads %d threads " % (getpid(), len(running_children)))
+            for running_child in running_children:
+                L.debug("Sending kill signal to %s" % running_child[1])
+                kill(running_child[1], 15)
 
 
 def clean_workunit(root_dag, proc):
